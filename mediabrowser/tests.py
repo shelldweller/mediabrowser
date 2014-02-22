24bd5e495d864cfa623b004f184bfb5e71488a06
Sergiy Kuzmenko <s.kuzmenko@gmail.com>
2014-02-22 17:49:39 -0500

    fixed broken test cases

--- mediabrowser/tests.py
+++ mediabrowser/tests.py
@@ -1,20 +1,55 @@
 from django.test import TestCase
 from django.test.client import Client
+from django.contrib.auth.models import User
+
 import os
 
-class StickyUrlParamsTestCase(TestCase):
-    def test_sticky_params_redirect(self):
-        """ Test that redirect contains the original GET params. """
-        
-        c = Client()
-        
-        filepath = os.path.join(os.path.dirname(__file__), 'static', 'mediabrowser', 'img', 'test.png')
-        with open(filepath) as fp:
-            response = c.post('/files/add/?type=Images&CKEditor=editor2&CKEditorFuncNum=2&langCode=de', {"file":fp})
-        
-        url = response['Location']
-        self.assertTrue('type=Images' in url)
-        self.assertTrue('CKEditor=editor2' in url)
-        self.assertTrue('CKEditorFuncNum=2' in url)
-        self.assertTrue('langCode=de' in url)
+
+
+IMAGE_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'testsite', 'img')
+
+
+class AuthenticatedUserTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create_user("testuser", password="s3kret")
+        user.is_staff = True
+        user.save()
     
+    def cleanUp(self):
+        #TODO: delete all uploaded images
+        pass
+    
+
+    def get_client(self):
+        c = Client()
+        c.login(username="testuser", password="s3kret")
+        return c
+
+    
+class FileBrowsingTestCase(AuthenticatedUserTestCase):
+    
+    def test_sticky_params_in_links(self):
+        """ Test that file browser links contains the original GET params. """
+        params = {
+            "type": "Images",
+            "CKEditor": "editor2",
+            "CKEditorFuncNum": "2",
+            "langCode": "en",
+        }
+        url = '/images/add/'
+        c = self.get_client()
+        response = c.get(url, params)
+        # we should really parse HTML and extract links instad of this:
+        for k,v in params.items():
+            self.assertContains(response, k+"="+v)
+
+
+class FileUploadTestCase(AuthenticatedUserTestCase):
+    
+    def test_image_upload(self):
+        c = self.get_client()
+        filepath =  os.path.join(IMAGE_ROOT, 'test.png')
+        with open(filepath) as fp:
+            response = c.post('/images/add/?type=Images&CKEditor=editor2&CKEditorFuncNum=2&langCode=de', {"file":fp})
+        self.assertTrue("asset" in response.context)
+        self.assertTrue("MEDIABROWSER.insertFile" in response.content, "Missing MEDIABROWSER.insertFile call in response")
--- testsite/testsite/settings.py
+++ testsite/testsite/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.auth',
     #'django.contrib.admin',
     'mediabrowser',
 
