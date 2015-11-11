3f93c03e928df5ac2d80076e8a9bc390d70ccc19
Sergiy Kuzmenko <s.kuzmenko@gmail.com>
2015-11-10 21:28:06 -0500

    tests for MEDIABROWSER_CHECK_USER_PERMISSIONS setting

--- mediabrowser/tests/tests.py
+++ mediabrowser/tests/tests.py
@@ -1,8 +1,11 @@
 from django.test import TestCase, override_settings
 from django.test.client import Client
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission
 from django.conf import settings
+from django.core.urlresolvers import reverse
 from PIL import Image
+
+from mediabrowser.models import Asset
 
 import os
 
@@ -11,7 +14,7 @@ import os
 IMAGE_ROOT = os.path.join(os.path.dirname(__file__), 'test-data')
 
 
-class AuthenticatedUserTestCase(TestCase):
+class AuthenticatedUserBaseTestCase(TestCase):
     def setUp(self):
         user = User.objects.create_user("testuser", password="s3kret")
         user.is_staff = True
@@ -29,7 +32,7 @@ class AuthenticatedUserTestCase(TestCase):
         return c
 
     
-class FileBrowsingTestCase(AuthenticatedUserTestCase):
+class FileBrowsingTestCase(AuthenticatedUserBaseTestCase):
     
     def test_sticky_params_in_links(self):
         """ Test that file browser links contains the original GET params. """
@@ -47,8 +50,9 @@ class FileBrowsingTestCase(AuthenticatedUserTestCase):
             self.assertContains(response, k+"="+v)
 
 
-class FileUploadTestCase(AuthenticatedUserTestCase):
+class FileUploadTestCase(AuthenticatedUserBaseTestCase):
     
+    @override_settings(MEDIABROWSER_CHECK_USER_PERMISSIONS=None)
     def test_image_upload(self):
         c = self.get_client()
         filepath =  os.path.join(IMAGE_ROOT, '40x40.png')
@@ -60,7 +64,7 @@ class FileUploadTestCase(AuthenticatedUserTestCase):
         self.assertTrue("MEDIABROWSER.insertFile" in response.content, "Missing MEDIABROWSER.insertFile call in response")
     
     
-    @override_settings(MEDIABROWSER_MAX_IMAGE_SIZE=(500,500))
+    @override_settings(MEDIABROWSER_MAX_IMAGE_SIZE=(500,500), MEDIABROWSER_CHECK_USER_PERMISSIONS=None)
     def test_image_resizing(self):
         client = self.get_client()
         
@@ -85,4 +89,82 @@ class FileUploadTestCase(AuthenticatedUserTestCase):
         self.assertEqual(500, asset.height)
         self.assertEqual(500, uploaded_img.size[0])
         self.assertEqual(500, uploaded_img.size[1])
+
+
+class AuthenticationTestCase(AuthenticatedUserBaseTestCase):
+    def post_image(self):
+        filepath =  os.path.join(IMAGE_ROOT, '40x40.png')
+        client = self.get_client()
+        with open(filepath) as fp:
+            response = client.post('/images/add/?type=Images', {"file":fp})
+        return response
+    
+    def update_user_perms(self, *codenames):
+        user = User.objects.get(username="testuser")
+        for codename in codenames:
+            user.user_permissions.add(Permission.objects.get(codename=codename, content_type__app_label="mediabrowser"))
         
+    @override_settings(MEDIABROWSER_CHECK_USER_PERMISSIONS=True, MEDIABROWSER_USER_PASSES_TEST=None)
+    def test_require_user_permissions(self):
+        """ Tests that if MEDIABROWSER_CHECK_USER_PERMISSIONS=True, user must have valid permissions to add or delete assets. """
+        
+        # by default user has no add_asset permission so request should be denied
+        response = self.post_image()
+        self.assertEqual(response.status_code, 403)
+        
+        # + add_asset permission
+        self.update_user_perms("add_asset")
+        response = self.post_image()
+        self.assertEqual(response.status_code, 200)
+        
+        # deleting an asset should result in 403 because user has no delete perm
+        asset = Asset.objects.all().order_by("-id")[0]
+        url = reverse("mediabrowser-delete")
+        response = self.get_client().post(url, {"asset_id":asset.id})
+        self.assertEqual(response.status_code, 403)
+        
+        # + delete_asset permission and now we can delete
+        self.update_user_perms('delete_asset')
+        response = self.get_client().post(url, {"asset_id":asset.id})
+        self.assertEqual(response.status_code, 200)
+    
+    @override_settings(MEDIABROWSER_CHECK_USER_PERMISSIONS=None, MEDIABROWSER_USER_PASSES_TEST=None)
+    def test_default_user_permissions(self):
+        """ Tests that default user permissions allow adding and deleting assets. """
+        
+        # upload image
+        response = self.post_image()
+        self.assertEqual(response.status_code, 200)
+        
+        # delete image
+        asset = Asset.objects.all().order_by("-id")[0]
+        url = reverse("mediabrowser-delete")
+        response = self.get_client().post(url, {"asset_id":asset.id})
+        self.assertEqual(response.status_code, 200)
+    
+    @override_settings(MEDIABROWSER_CHECK_USER_PERMISSIONS=None, MEDIABROWSER_USER_PASSES_TEST=None)
+    def test_template_with_default_permissions(self):
+        """ Tests that with default user permissions upload and delete buttons are present. """
+        upload_url = reverse("mediabrowser-add-image")
+        response = self.get_client().get(reverse("mediabrowser-image-list"))
+        self.assertIn(upload_url, response.content)
+        self.assertIn('id="DeleteFile"', response.content)
+    
+    @override_settings(MEDIABROWSER_CHECK_USER_PERMISSIONS=True, MEDIABROWSER_USER_PASSES_TEST=None)
+    def test_template_with_default_permissions(self):
+        """ Tests delete and upload links are only present if user has apropriate permissions. """
+        upload_url = reverse("mediabrowser-add-image")
+        
+        # if user has no permission, delete and upload links should not be there
+        response = self.get_client().get(reverse("mediabrowser-image-list"))
+        self.assertNotIn(upload_url, response.content)
+        self.assertNotIn('id="DeleteFile"', response.content)
+        
+        # after updating permissions, both links should become visible
+        self.update_user_perms("add_asset", "delete_asset")
+        response = self.get_client().get(reverse("mediabrowser-image-list"))
+        self.assertIn(upload_url, response.content)
+        self.assertIn('id="DeleteFile"', response.content)
+        
+    
+    
